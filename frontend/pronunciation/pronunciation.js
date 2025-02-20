4cf09a7615f2b99b63bb55c897dc4cd3a2b44c6a
Larry Grullon-Polanco <larrygrpolanco@gmail.com>
2025-02-20 21:53:03 +0800

    Sveltekit

--- frontend/pronunciation/pronunciation.js
+++
@@ -1,5 +0,0 @@
-// This will hold our interactive chart logic
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Pronunciation guide loaded');
-  // We'll add our interactive chart code here tomorrow
-});
--- frontend/script.js
+++
@@ -1,4 +0,0 @@
-// We'll add our interactive functionality here
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Application loaded');
-});
